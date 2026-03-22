23f48932e8034f680ed71166480e98c9b945ed7d
Guilherme Lima <guilimadev19@email.com>
2026-03-22 12:26:11 -0300

    feat : created all models controllers

---
+++ src/main/java/LimaSantosSoftware/DriveControl/controller/driverController.java
@@ -0,0 +1,15 @@
+package LimaSantosSoftware.DriveControl.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+
+public class driverController {
+    //add drivers (create)
+    //search drivers for id (read)
+    //show all drivers(read)
+    //update drivers data (update)
+    //delete drivers (delete)
+}
--- src/main/java/LimaSantosSoftware/DriveControl/repository/RentalRepository.java
+++ src/main/java/LimaSantosSoftware/DriveControl/repository/RentalRepository.java
@@ -7,4 +7,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RentalRepository extends JpaRepository<Rental, Long> {
 
-}q
+}
